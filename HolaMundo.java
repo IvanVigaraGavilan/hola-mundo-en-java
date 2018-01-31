fc92b1e6b48bdecb7222df4b3a9fddd8028c7f9d
mrequena <fakespam76@gmail.com>
2018-01-31 13:42:38 +0100

    saludo ampliado

--- HolaMundo.java
+++ HolaMundo.java
@@ -8,7 +8,7 @@
 public class HolaMundo {
      
     	public static void main(String[] args) {		
-    		System.out.println("Hola Mundo");
+    		System.out.println("Hola Mundo en Marmolejo");
     	}
      
     }
