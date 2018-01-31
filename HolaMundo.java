562cc8da1e550c4051a41ce20d25b7bec72128ed
Iván Vigara Gavilán <35997716+IvanVigaraGavilan@users.noreply.github.com>
2018-01-31 14:15:12 +0100

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -11,6 +11,7 @@ String azul = "\033[34m";
      
     	public static void main(String[] args) {		
     		System.out.println("Hola Mundo en Marmolejo");
+          System.out.printf("Hola desde el Sotillo")
     	}
      
     }
