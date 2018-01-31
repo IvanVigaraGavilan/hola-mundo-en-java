f9ae6cac2b9875ee79f9ac0453504bcce90f1f4c
mrequena <fakespam76@gmail.com>
2018-01-31 13:50:53 +0100

    colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,6 +6,8 @@
 
 
 public class HolaMundo {
+String naranja = "\033[33m";
+String azul = "\033[34m";
      
     	public static void main(String[] args) {		
     		System.out.println("Hola Mundo en Marmolejo");
